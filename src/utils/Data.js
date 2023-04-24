79706df9a901aa6c8d35438ac7c44c51f53b34a0
Aplo0x <aplo0x764@gmail.com>
2023-04-24 17:13:46 -0300

    about page created

---
+++ src/utils/Data.js
@@ -0,0 +1,39 @@
+export const data = [
+  {
+    subject: "Pentest",
+    A: 100,
+    B: 70,
+    fullMark: 100,
+  },
+  {
+    subject: "Front End",
+    A: 80,
+    B: 30,
+    fullMark: 100,
+  },
+  {
+    subject: "Mobile",
+    A: 30,
+    B: 10,
+    fullMark: 100,
+  },
+  {
+    subject: "Back End",
+    A: 70,
+    B: 10,
+    fullMark: 100,
+  },
+  {
+    subject: "Data Science",
+    A: 40,
+    B: 10,
+    fullMark: 100,
+  },
+];
+
+export const aboutMe = [
+  {
+    head: "Who Am I",
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
