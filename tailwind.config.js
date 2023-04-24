685c9d21a44a04b0afba367e3e4934789ccf1145
Aplo0x <aplo0x764@gmail.com>
2023-04-24 15:59:24 -0300

    project setup and custom colors and assets added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        red: '#ff003c',
+        solid: '#121211',
+        gray: '#12141f',
+        orange: '#F06520',
+        yellow: '#FC8E06',
+        blue: '#00EDFF',
+      }
     },
   },
   plugins: [],
